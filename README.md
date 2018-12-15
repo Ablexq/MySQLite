6e9e0d6f336fe03ca4e4698200e8e9f7d5b564b7
ablexq <776356314@qq.com>
2018-12-15 11:42:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[慕课网：Android基础教程-SQLite高级操作](http://www.imooc.com/course/qa/id/749/t/0?page=3)
---
+++ app/src/main/java/com/xq/mysqlite/util/Constant.java
@@ -0,0 +1,10 @@
+package com.xq.mysqlite.util;
+
+public class Constant {
+    public static final String DATABASE_NAME = "info.db";
+    public static final int DATA_VERSION = 1;
+    public static final String TABBLE_NAME = "person";
+    public static final String ID = "_id";
+    public static final String NAME = "name";
+    public static final String AGE = "age";
+}
